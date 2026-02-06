b7d5cfbe34a922e5a2913952f2b115637aa70095
王俐涵 <zzkuaikuai@gmail.com>
2026-02-06 23:59:03 +0800

    pages環境建置

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react_project_week4/",
+  base: "/react_project_week5/",
   plugins: [react()],
 })
